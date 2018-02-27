382b140b3f20e3726c352d06278c79d2253437ee
Lutz Helm <helm@ub.uni-leipzig.de>
2018-02-27 15:51:01 +0100

    Add simple 'classloader' file for non-composer projects

---
+++ src/iiif/classloader.php
@@ -0,0 +1,25 @@
+<?php
+
+// Use this file if you don't want to or can't rely on composer or other autoloaders
+
+require_once(__DIR__."/model/constants/ViewingDirectionValues.php");
+require_once(__DIR__."/model/helper/IiifReader.php");
+require_once(__DIR__."/model/properties/FormatTrait.php");
+require_once(__DIR__."/model/properties/NavDateTrait.php");
+require_once(__DIR__."/model/properties/ViewingDirectionTrait.php");
+require_once(__DIR__."/model/properties/WidthAndHeightTrait.php");
+require_once(__DIR__."/model/resources/AbstractIiifResource.php");
+require_once(__DIR__."/model/resources/Annotation.php");
+require_once(__DIR__."/model/resources/AnnotationList.php");
+require_once(__DIR__."/model/resources/Canvas.php");
+require_once(__DIR__."/model/resources/Collection.php");
+require_once(__DIR__."/model/resources/ContentResource.php");
+require_once(__DIR__."/model/resources/Layer.php");
+require_once(__DIR__."/model/resources/Manifest.php");
+require_once(__DIR__."/model/resources/Range.php");
+require_once(__DIR__."/model/resources/Sequence.php");
+require_once(__DIR__."/model/resources/Service.php");
+require_once(__DIR__."/model/vocabulary/MiscNames.php");
+require_once(__DIR__."/model/vocabulary/Names.php");
+require_once(__DIR__."/model/vocabulary/Types.php");
+
