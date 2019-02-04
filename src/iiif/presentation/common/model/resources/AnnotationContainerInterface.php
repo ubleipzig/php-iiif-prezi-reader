21d1824e5468f503c18f3eb5ff5f0f0020b1f076
Lutz Helm <helm@ub.uni-leipzig.de>
2019-02-04 15:16:03 +0100

    Documentation

--- src/iiif/presentation/common/model/resources/AnnotationContainerInterface.php
+++ src/iiif/presentation/common/model/resources/AnnotationContainerInterface.php
@@ -1,10 +1,16 @@
 <?php
 namespace iiif\presentation\common\model\resources;
 
+
 interface AnnotationContainerInterface extends IiifResourceInterface {
     
     /**
-     * @return AnnotationInterface[]
+     * @param $motivation string The name of an instance of https://www.w3.org/ns/oa#Motivation,
+     * usually "painting", respectively "sc:painting", or "oa:commenting".
+     * @return AnnotationInterface[] All text annotations with the given $motivation,
+     * or all text annotations if $motivation is null.
+     * @link https://www.w3.org/TR/annotation-vocab/#motivation
+     * 
      */
     public function getTextAnnotations($motivation = null);
     
