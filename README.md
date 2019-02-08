30f991902310442ff2bc0fe6a4d39b40cedf0ddb
Lutz Helm <helm@ub.uni-leipzig.de>
2019-02-08 13:29:44 +0100

    Doc, Readme

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # IIIF Manifest Reader for PHP
 
-Objective of this project is to load IIIF Presentation API manifests into accessible PHP objects.
+Read IIIF Presentation API resources into PHP objects that offer some convenient data extraction methods.
 
 Requires at least PHP 5.6 at the moment.
 
 ## Where to start
 
 * Any IIIF manifest (or any other IIIF Presentation API / Metadata API resource json document) as text content, URL or path can be loaded into a PHP object representing the document with `\iiif\tools\IiifHelper::loadIiifResource($resource)`. The object will contain all "child" resources in a property with the name of their properties in the IIIF Metadata API 1 (namespace `iiif\presentation\v1\model\resources`), IIIF Presentation API 2 (`iiif\presentation\v2\model\resources`) or IIIF Presentation API 3 (`iiif\presentation\v3\model\resources`). You can also use most of these resources via interfaces that are defined in `iiif\presentation\common\model\resources`. These interfaces attempt to offer a (somewhat reduced) version independent funtionallity.  
-* If you want to use any framework's functionality for retrieving remote documents instead of PHP's `file_get_contents()`, implement `\iiif\tools\UrlReaderInterface` and use `\iiif\tools\IiifHelper::setUrlReader(UrlReaderInterface $urlReader)` before loading any documents. 
+* If you want to use any framework's functionality for retrieving remote documents instead of PHP's `file_get_contents()`, implement `\iiif\tools\UrlReaderInterface` and use `\iiif\tools\IiifHelper::setUrlReader(UrlReaderInterface $urlReader)` before loading any documents.
+* Use `\iiif\tools\IiifHelper::setMaxThumbnailWidth($maxThumbnailWidth)` and `\iiif\tools\IiifHelper::setMaxThumbnailHeight($maxThumbnailHeight)` to set the maximum dimensions for resource thumbnails that offer an IIIF image service for generating thumbnails.
 
 ## TODOs:
 
--- src/iiif/tools/IiifHelper.php
+++ src/iiif/tools/IiifHelper.php
@@ -5,10 +5,24 @@ use iiif\presentation\common\model\AbstractIiifEntity;
 
 class IiifHelper {
 
+    /**
+     * 
+     * @param string|array $resource IIIF resource. Can be an IRI, the JSON document as string
+     * or a dictionary in form of a PHP associative array 
+     * @return NULL|\iiif\presentation\common\model\AbstractIiifEntity An instance of the IIIF 
+     */
     public static function loadIiifResource($resource) {
         return AbstractIiifEntity::loadIiifResource($resource);
     }
 
+
+    /**
+     * Retrieve a document by its URL either via file_get_contents() or via an instance of
+     * UrlReaderInterface that has previously been set with IiifHelper::setUrlReader(). Primarily
+     * use is library internal. 
+     * @param string $url URL of the requested resource
+     * @return null|string Text content of the requested resource or null
+     */
     public static function getRemoteContent($url) {
         if (Options::getUrlReader() != null) {
             return Options::getUrlReader()->getContent($url);
@@ -16,14 +30,28 @@ class IiifHelper {
         return file_get_contents($url);
     }
     
+    /**
+     * Offers a way to use any framework logic to retrieve remote documents instead of just using
+     * file_get_contents(). Implement UrlReaderInterface and call this method before loading any
+     * remote IIIF documents
+     * @param UrlReaderInterface $urlReader
+     */
     public static function setUrlReader(UrlReaderInterface $urlReader = null) {
         Options::setUrlReader($urlReader);
     }
     
+    /**
+     * Set the maximum width for thumbails that offer a IIIF image service to generate thumbnails.
+     * @param int $maxThumbnailWidth
+     */
     public static function setMaxThumbnailWidth($maxThumbnailWidth) {
         Options::setMaxThumbnailWidth($maxThumbnailWidth);
     }
 
+    /**
+     * Set the maximum height for thumbails that offer a IIIF image service to generate thumbnails.
+     * @param int $maxThumbnailHeight
+     */
     public static function setMaxThumbnailHeight($maxThumbnailHeight) {
         Options::setMaxThumbnailHeight($maxThumbnailHeight);
     }
