7e87fdba59cf208dd7d994309e5bc826e38b4d46
Lutz Helm <helm@ub.uni-leipzig.de>
2019-02-13 10:47:04 +0100

    Reorganize context files

--- tests/iiif/context/ContextResourceTest.php
+++ tests/iiif/context/ContextResourceTest.php
@@ -7,17 +7,20 @@ use PHPUnit\Framework\TestCase;
  */
 class ContextResourceTest extends TestCase {
     
+    /**
+     * @var array Local conbtext
+     */
     const CONTEXTS = [
-        "http://www.w3.org/ns/anno.jsonld" => "annotation-context.json",
-        "http://iiif.io/api/auth/1/context.json" => "auth-context-1.json",
-        "http://iiif.io/api/image/1/context.json" => "image-context-1.json",
-        "http://iiif.io/api/image/2/context.json" => "image-context-2.json",
-        "http://iiif.io/api/image/3/context.json" => "image-context-3.json",
-        "http://iiif.io/api/presentation/3/combined-context.json" => "presentation-combined-context-3.json",
-        "http://iiif.io/api/presentation/1/context.json" => "presentation-context-1.json",
-        "http://iiif.io/api/presentation/2/context.json" => "presentation-context-2.json",
-        "http://iiif.io/api/presentation/3/context.json" => "presentation-context-3.json",
-        "http://iiif.io/api/search/1/context.json" => "search-context-1.json"
+        "http://www.w3.org/ns/anno.jsonld" => "annotation/annotation-context.json",
+        "http://iiif.io/api/auth/1/context.json" => "iiif/auth-context-1.json",
+        "http://iiif.io/api/image/1/context.json" => "iiif/image-context-1.json",
+        "http://iiif.io/api/image/2/context.json" => "iiif/image-context-2.json",
+        "http://iiif.io/api/image/3/context.json" => "iiif/image-context-3.json",
+        "http://iiif.io/api/presentation/3/combined-context.json" => "iiif/presentation-combined-context-3.json",
+        "http://iiif.io/api/presentation/1/context.json" => "iiif/presentation-context-1.json",
+        "http://iiif.io/api/presentation/2/context.json" => "iiif/presentation-context-2.json",
+        "http://iiif.io/api/presentation/3/context.json" => "iiif/presentation-context-3.json",
+        "http://iiif.io/api/search/1/context.json" => "iiif/search-context-1.json"
     ];
 
     /**
